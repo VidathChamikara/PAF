1abd04038c552b991d6eea2bd0656a39c7fe761b
Jayathunga T.M <79472177+IT20146238@users.noreply.github.com>
2022-04-23 19:32:10 +0530

    Create project structure

---
+++ ElectroGridSystem/src/main/java/com/billManagement/model/Bill.java
@@ -0,0 +1,22 @@
+package com.billManagement.model;
+
+public class Bill {
+	
+	private int bill_id;
+	private int account_no;
+	private String name;
+	private String address;
+	private String previous_date;
+	private int previous_reading;
+	private String current_date;
+	private int current_reading;
+	private int units;
+	private double current_amount;
+	private double previous_ammount;
+	private double total_ammount;
+	
+	
+	
+	
+
+}
